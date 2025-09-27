04f14f104bbbf7041eaf77e2b689156dd42058e7
yuki.maenosono <137788728+marufoi22@users.noreply.github.com>
2025-09-27 21:40:22 +0900

    feat: テトリスゲームの基本機能を実装
    - ゲームボードの描画機能を追加
    - テトロミノの定義と描画機能を実装
    - ゲームループとブロック落下機能を追加
    - 移動ボタン（左、右、回転）をUIに追加
    - レイアウトを縦方向に最適化
    - scriptタグにdefer属性を追加して読み込み順序を制御

--- js/board.js
+++ js/board.js
@@ -1,1 +1,51 @@
-// ボードのロジック
+// ボード
+
+// 幅と高さは、HTMLのスタイルに合わせる
+// 幅 10 * 30 = 300px
+const COLUMNS = 10;
+// 高さ 20 * 30 = 600px
+const ROWS = 20;
+//　 1マス分の幅・高さ
+const BLOCK_SIZE = 30;
+
+// 盤面配列を作成
+let board = [];
+for(let r = 0; r < ROWS; r++){
+    board[r] = [];
+    for(let c = 0; c < COLUMNS; c++){
+        board[r][c] = 0;
+    }
+}
+
+// canvas取得
+const canvas = document.querySelector('#main-canvas');
+const context = canvas.getContext('2d');
+
+// グローバル変数として公開
+window.gameContext = context;
+
+// 描画関数
+// fillRect：塗りつぶした四角形を描く
+// strokeRect:枠線だけの四角形を描く
+const drawBoard = () => {
+    // 背景全体を灰色に塗る
+    context.fillStyle = 'lightgray'
+    context.fillRect( 0, 0, canvas.width, canvas.height);
+
+    for(let r = 0; r < ROWS; r++){
+        for(let c = 0; c < COLUMNS; c++){
+            if(board[r][c] !== 0){
+                context.fillStyle = 'blue';
+                context.fillRect(c * BLOCK_SIZE, r * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+            } else {
+                context.fillStyle = 'lightgray';
+                context.fillRect(c * BLOCK_SIZE, r * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+                context.strokeStyle = '#f5f5f5';
+                context.strokeRect(c * BLOCK_SIZE, r * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+            }
+        }
+    }
+}
+
+// グローバル関数として公開
+window.drawBoard = drawBoard;
--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 // ゲーム起動・ループ
+
+// ページ読み込み時に初期画面を描画
+window.addEventListener('load', () => {
+    window.drawBoard();
+});
+
+const startButton = document.querySelector("#start-button");
+const pauseButton = document.querySelector("#pause-button");
+
+// タイマー宣言
+// TODO:一時停止ボタンを押した時に、dropIntervalを止めたら、ミノの停止も止まるようにしたい。
+let dropInterval;
+
+startButton.addEventListener('click', () => {
+    // 既にタイマーが動いていたら一旦クリア
+    if(dropInterval) clearInterval(dropInterval);
+
+    // 500msごとにブロックを落とす
+    dropInterval = setInterval(() => {
+        window.drawBoard();
+        window.drop();
+    }, 500);
+
+    // ボタン表示切り替え
+    startButton.classList.add('d-none');
+    pauseButton.classList.remove('d-none');
+})
