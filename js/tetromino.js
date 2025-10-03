31b7ee5d811a5ae9aac2d4181b0cc1f3074b9758
imajin5656 <imajin5656@users.noreply.github.com>
2025-10-04 08:37:38 +0900

    add stop-mino feature

--- js/tetromino.js
+++ js/tetromino.js
@@ -65,33 +65,52 @@ const checkCollision = () => {
 
     // blockの底辺がboardの底辺を超える場合はture
     if(row + height >= window.board.length) {
-        console.log(`collision : over boards height`);
+        // console.log(`collision : over boards height`);
         return true;
     }
 
     for(let h = height; h > 0; h--){
         for(let w = width; w > 0; w--){
             // currentBlockのいづれかのマスが他のブロックに衝突する場合はtrue
-            if(currentBlock.shape[y][x] !== 0 && window.board[row + h][col] !== 0) {
-                console.log(`collision : collision with other blocks`);
+            if(currentBlock.shape[y][x] == 1 && window.board[row + 1][col] == 1) {
+                // console.log(`collision : collision with other blocks`);
+                // console.log(`blockPos : y = ${row}, x = ${col} ,value = ${currentBlock.shape[y][x]}`);
+                // console.log(`blockCell : y = ${x}, x = ${y} ,value = ${currentBlock.shape[y][x]}`);
+                // console.log(`boardPos : y = ${row + 1}, x = ${col} ,value = ${window.board[row + 1][col]}`);
                 return true;
             }
             col++;
             x++;
         }
+        // 次の行へ移る際にパラメータをリセット
         col = currentBlock.col;
+        x = 0;
         row++;
         y++;
     }
-    console.log("No Collision");
+    //console.log("No Collision");
     return false;
+}
+
+const stop = () => {
+    let row = currentBlock.row;
+    let col = currentBlock.col;
+    for(let r = 0; r < currentBlock.shape.length; r++){
+        for(let c = 0; c < currentBlock.shape[0].length; c++){
+            if(currentBlock.shape[r][c] == 1) window.board[row+r][col+c] = 1;
+        }
+    }
 }
 
 const drop = () => {
     currentBlock.row++;
     window.drawBoard();
     drawBlock();
-    checkCollision();
+    if(checkCollision()){
+        stop();
+        drawBlock();
+        currentBlock = getRandomBlock();
+    }
 }
 
 // グローバル関数として公開
