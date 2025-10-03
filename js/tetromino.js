f7c824002c4370b4d9418050b45fd6d3cf235a45
imajin5656 <imajin5656@users.noreply.github.com>
2025-10-03 22:14:52 +0900

    add checkCollision function

--- js/tetromino.js
+++ js/tetromino.js
@@ -55,10 +55,43 @@ const drawBlock = () => {
     });
 }
 
+const checkCollision = () => {
+    let row = currentBlock.row;
+    let col = currentBlock.col;
+    let height = currentBlock.shape.length;
+    let width = currentBlock.shape[0].length;
+    let y = 0;
+    let x = 0;
+
+    // blockの底辺がboardの底辺を超える場合はture
+    if(row + height >= window.board.length) {
+        console.log(`collision : over boards height`);
+        return true;
+    }
+
+    for(let h = height; h > 0; h--){
+        for(let w = width; w > 0; w--){
+            // currentBlockのいづれかのマスが他のブロックに衝突する場合はtrue
+            if(currentBlock.shape[y][x] !== 0 && window.board[row + h][col] !== 0) {
+                console.log(`collision : collision with other blocks`);
+                return true;
+            }
+            col++;
+            x++;
+        }
+        col = currentBlock.col;
+        row++;
+        y++;
+    }
+    console.log("No Collision");
+    return false;
+}
+
 const drop = () => {
     currentBlock.row++;
     window.drawBoard();
     drawBlock();
+    checkCollision();
 }
 
 // グローバル関数として公開
